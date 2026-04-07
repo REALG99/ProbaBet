e60cb4aa6f898e37dc8d597afeb7f90a7131d6d2
REALG99 <mindup98@gmail.com>
2026-04-07 20:07:43 +0200

    Delete dashboard directory

--- dashboard/page.tsx
+++
@@ -1,6 +0,0 @@
-import { matches } from '../../data/matches';
-import DashboardClient from './DashboardClient';
-
-export default function DashboardPage() {
-  return <DashboardClient fixtures={matches} />;
-}
